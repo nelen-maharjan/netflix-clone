005b2e6958f08fe3b1c67fdb2ef3728d52626f0b
Nelen Maharjan <neleen11@gmail.com>
2024-11-07 21:17:45 +0545

    starter folders

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className="home">Home</div>
+  )
+}
+
+export default Home
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,9 @@
+import './player.css'
+
+const Player = () => {
+  return (
+    <div>Player</div>
+  )
+}
+
+export default Player
