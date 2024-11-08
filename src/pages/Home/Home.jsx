791f7505ad2664a7ddb2d16fbd7dbb46c4031b7c
Nelen Maharjan <neleen11@gmail.com>
2024-11-08 19:37:27 +0545

    hero-section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,30 @@
 import './home.css'
 import Navbar from '../../components/Navbar/Navbar';
-// import hero_banner from '../../assets/hero_banner.jpg'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <div className="hero">
-        {/* <img src={hero_banner} alt="hero" /> */}
+        <img src={hero_banner} alt="hero" className='banner-img' />
+        <div className="hero-caption">
+          <img src={hero_title} alt="title" className='caption-img' />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+          <div className="hero-btns">
+            <button className='btn'>
+              <img src={play_icon} alt="play" />
+              Play
+            </button>
+            <button className='btn dark-btn'>
+              <img src={info_icon} alt="play" />
+              More Info
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
