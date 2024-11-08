b1a5bb816929a8d2c1298f31d0cb177e7472dfce
Nelen Maharjan <neleen11@gmail.com>
2024-11-08 19:01:36 +0545

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,35 @@
 import './navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} alt="search-icon" />
+        <p>Children</p>
+        <img src={bell_icon} alt="bell-icon" />
+        <div className="navbar-profile">
+        <img src={profile_img} alt="user" className='profile' />
+        <img src={caret_icon} alt="icon" />
+
+        </div>
+      </div>
+    </div>
   )
 }
 
