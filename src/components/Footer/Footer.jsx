bf99bfbeeeaf3244d40a309f2ab6cc0083063935
Nelen Maharjan <neleen11@gmail.com>
2024-11-08 21:41:00 +0545

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,34 @@
 import './footer.css'
+import facebook_icon from '../../assets/facebook_icon.png'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="footer-icons">
+        <img src={facebook_icon} alt="facebook" />
+        <img src={instagram_icon} alt="instagram" />
+        <img src={twitter_icon} alt="twitter" />
+        <img src={youtube_icon} alt="youtube" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relation</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notice</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>&copy; 1997-2024 Netflix, Inc.</p>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/TitleCards';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
@@ -34,6 +35,7 @@ const Home = () => {
         <TitleCards title={'Upcoming'} />
         <TitleCards title={'Top Pics for you'} />
       </div>
+      <Footer/>
     </div>
   )
 }
