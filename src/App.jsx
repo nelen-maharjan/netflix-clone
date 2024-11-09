61045f9b68bf248400a6b985f6d8448d01c2731c
Nelen Maharjan <neleen11@gmail.com>
2024-11-09 08:48:16 +0545

    react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Home from './pages/Home/Home'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 function App() {
 
   return (
     <>
-      <Home />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
+      
     </>
   )
 }
