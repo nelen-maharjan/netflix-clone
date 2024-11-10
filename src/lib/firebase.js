9d044b766dfa64348660259fb4c155660674ebc7
Nelen Maharjan <neleen11@gmail.com>
2024-11-10 12:11:44 +0545

    login & logout function

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -32,11 +32,17 @@ const signup = async (name, email, password) => {
     }
 }
 
-const login = async(email, password) => {
+const login = async (email, password) => {
     try {
-        
+        await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
         console.log(error);
-        
+        alert(error);
     }
 }
+
+const logout = () => {
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
