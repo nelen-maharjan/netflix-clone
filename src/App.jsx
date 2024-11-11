6225a1f6ba18bfd85c0803d3333f337b464b2097
Nelen Maharjan <neleen11@gmail.com>
2024-11-11 14:10:56 +0545

    react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import Home from './pages/Home/Home'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { auth } from './lib/firebase'
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, async(user) => {
+      if(user){
+        console.log('Logged In');
+        navigate('/')
+      }else{
+        console.log('Logged Out');
+        navigate('/login')
+      }
+    });
+  }, [])
 
   return (
     <>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import bell_icon from '../../assets/bell_icon.svg'
 import profile_img from '../../assets/profile_img.png'
 import caret_icon from '../../assets/caret_icon.svg'
 import { useEffect, useRef } from 'react'
+import { logout } from '../../lib/firebase'
 
 const Navbar = () => {
 
@@ -41,7 +42,7 @@ const Navbar = () => {
         <img src={profile_img} alt="user" className='profile' />
         <img src={caret_icon} alt="icon" />
         <div className="dropdown">
-          <p>Sign out of Netflix</p>
+          <p onClick={()=>{logout()}}>Sign out of Netflix</p>
         </div>
         </div>
       </div>
